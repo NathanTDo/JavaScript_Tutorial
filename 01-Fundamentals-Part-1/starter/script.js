176411d98a7e10628775e6011e6d64b546b82783
Nathan Do <nathando@nathans-air.lan>
2021-09-03 19:10:21 -0700

    Finished coding challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,55 @@
+/*
 let js = "amazing";
-if (js === 'amazing') alert('JavaScript if Fun!');
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Jonas";
+let first = "jonas";
+
+let person = "jonas";
+let pi = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurretnJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'true');
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+let age = 30;
+age =  31;
+
+const birthYear = 1991;
+*/
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 20;
+const johnHeight = 1.95;
+const johnBMI = johnMass / johnHeight ** 2
+const markBMI = markMass / markHeight ** 2
+const markHigherBMI = (markBMI > johnBMI);
+
+if (markHigherBMI === true) {
+    console.log("Mark's BMI is higher than John's!")
+} 
+else { 
+     console.log("John's BMI is higher than Mark's!") 
+}
+
+console.log(markHigherBMI);
