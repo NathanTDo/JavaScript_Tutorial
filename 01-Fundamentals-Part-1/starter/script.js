1fb0187105c1c782f762382488e8378935f59314
Nathan Do <nathando@nathans-air.egusd.net>
2021-09-28 14:57:49 -0700

    saving

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -120,6 +120,22 @@ if (favourite === 23) { // '23' = 23
 //     console.log('Someone else should drive...');
 // }
 
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// const scoreDolphins = calcAverage(0, 23 ,71);
+// const scoreKoalas = calcAverage(650, 54, 49);
+
+// function checkWinner(avgDolphins, avgKoalas) {
+//     if (avgDolphins >= avgKoalas * 2) {
+//         console.log(`Dolphins Win ${avgDolphins} vs. ${avgKoalas}`);
+//     } else if (avgKoalas >= avgDolphins * 2) {
+//         console.log(`Koalas Win ${avgKoalas} vs. ${avgDolphins}`);
+//     } else {
+//         console.log("Nobody wins");
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
 // const scoreDolphins = (97 + 112 + 101) / 3;
 // const scoreKoalas = (109 + 95 + 106) / 3;
 
@@ -133,27 +149,93 @@ if (favourite === 23) { // '23' = 23
 //     console.log('nobody wins')
 // }
 
-const day = 'thursday';
+// const day = 'thursday';
 
-switch(day) {
-    case 'monday':
-        console.log('Plan couse structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break;
-    case 'friday':
-        console.log('Record videos');
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend!');
-        break;
-    default:
-        console.log('Not a valid day!')
+// switch(day) {
+//     case 'monday':
+//         console.log('Plan couse structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend!');
+//         break;
+//     default:
+//         console.log('Not a valid day!')
+// }
+
+// const age = 15;
+// // age >= 18 ? console.log('I like to drink wine') : 
+// // console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// let drink2;
+// if (age >- 18) {
+//     drink2 = 'wine';
+// } else {
+//     drink2 = 'water';
+// }
+
+// console.log(`I like to drink ${age >= 18 ? 'wine'
+//  : 'water'}`);
+
+// function totalBillCalculator(bill) {
+//     const total = (bill >= 50 && bill <= 300) ? 1.15 * bill : 
+//     1.2 * bill;
+//     console.log(total);
+//     return total;
+// }
+// const bill = totalBillCalculator(275);
+
+// function totalBillCalculator(bill) {
+//     let total;
+//     if (bill >= 50 && bill <= 300) {
+//         total = 1.15 * bill;
+//     } else {
+//         total = 1.2 * bill
+//     }
+//     return total;
+// }
+
+
+// const bill = totalBillCalculator(10);
+// console.log(bill);
+
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} apple and ${orangePieces} oranges.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2,3));
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
 }
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+console.log(calcAge(years[2]))
